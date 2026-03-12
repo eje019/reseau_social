41422241c5366d665030a30ba96660694bac61e6
eje019 <emricndayake@gmail.com>
2026-03-12 03:33:16 +0000

    Page d'inscription

--- inscription.php
+++ inscription.php
@@ -2,3 +2,15 @@
 require_once("includes/db.php")
 
 ?>
+
+<!DOCTYPE html>
+<html lang = "en"></html>
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>Inscription</h1>
+    <form action=""></form>
+</body>
