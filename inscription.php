9931226e29a12f1f0af8bd8a817ba1453cd25b50
eje019 <emricndayake@gmail.com>
2026-03-12 11:04:54 +0000

    Feat : formulaire d'inscription

--- inscription.php
+++ inscription.php
@@ -12,5 +12,29 @@ require_once("includes/db.php")
 </head>
 <body>
     <h1>Inscription</h1>
-    <form action=""></form>
+    <form action="inscription.php" methode="POST">
+        <div>
+            <label for="nom_utilisateur">Nom d'utilisateur :</label>
+            <input type="text" id="nom_utilisateur" name="nom_utilisateur" required>
+        </div>
+
+        <div>
+            <label for="email">Email :</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+
+        <div>
+            <label for="password">Mot de asse :</label>
+            <input type="password"name="password" id="password" required>
+        </div>
+
+        <div>
+            <label for="password">Confirmer le mot de passe :</label>
+            <input type="password"name="confirmation" id="confirmation" required>
+        </div>
+
+        <button type="submit">S'incrire</button>
+    </form>
+
+    <p>Deja inscrit ? <a href="connecion.php">Connectez vous ici</a></p>
 </body>
